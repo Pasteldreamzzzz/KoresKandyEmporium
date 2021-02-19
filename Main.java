1b2c3ef804addd5153cdd3b234d6e027386cb014
Pasteldreamzzzz <gabrielle.paul@patelhs.org>
2021-02-19 14:58:14 +0000

    Welcome message and scanner

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+   
+   System.out.println("Welcome to Kore's Kandy Emporium!"); 
+   
+   Scanner order = new Scanner(System.in); 
+   
+   System.out.println("What is your order number?");
+   String number = order.next();
+    
+   System.out.println(number);
+
+
+
+    
   }
 }
