2f620cf6b10a8f020a925ef403f9db03e1e21df7
Pasteldreamzzzz <gabrielle.paul@patelhs.org>
2021-02-27 03:21:25 +0000

    OH MY GOD ITS RUNNING THANK THE LORD ITS RUNNING THAT LITERALLY TOOK ME A WHOLE WEEK TO NOTICE THAT I DIDNT MOVE THAT STUPID CURLY BRACKET

--- Main.java
+++ Main.java
@@ -10,9 +10,18 @@ class Main {
    int order = number.nextInt();
     
    System.out.println(order);
-
-
+   }
+   
+   static void pickup(int order){
+    
+   if(order == 5567)
+   {
+   System.out.println("Yay! Enjoy your food! Are you picking up another order?");
+   }else{
+     System.out.println("Hmmm... I'm sorry I don't see that order in my database"); 
+   }
+   }
 
     
-  }
+  
 }
